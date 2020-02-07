5d2989ae0e7b6035f8bb939f4777027958d2dbf0
Lazar Marković <lazar.kmarkovic@gmail.com>
2020-02-07 19:17:10 +0100

    Add unordered lists

--- akashic/main.py
+++ akashic/main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     # Read rule from sample file
     this_folder = dirname(__file__)
-    sample_path = join(this_folder, 'test_samples', 'sample2_alone.json')
+    sample_path = join(this_folder, 'test_samples', 'sample1.json')
     with open(sample_path, 'r') as sample:
         akashic_rule = sample.read()
         i.interpret(akashic_rule)
