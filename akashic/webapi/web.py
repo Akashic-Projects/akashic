43e64517905d1670c8efdc8b0affeb9a189efaf2
Lazar Marković <lazar.kmarkovic@gmail.com>
2020-05-11 22:52:15 +0200

    Add simple insert_one method to test mongodb - IT IS COOOL

--- akashic/webapi/web.py
+++ akashic/webapi/web.py
@@ -1,29 +1,22 @@
-from flask import Flask
-from flask import jsonify
-from flask import request
+from flask import Flask, jsonify, request 
 from flask_pymongo import PyMongo
-
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-app.config["MONGO_URI"] = "mongodb://admin:devdevdev@127.0.0.1:27017/test?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+app.config["MONGO_URI"] = "mongodb://admin:devdevdev@127.0.0.1:27017/akashic?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 
 mongo = PyMongo(app)
 
 @app.route('/rules', methods=['POST'])
 def create_rule():
-  content = request.json
-  print(content)
-  return content
+  akashic_rule = request.json
+  akashic_rule['_id'] = akashic_rule['rule-name']
 
-  # user = mongo.db.user
-  # output = []
-  # print(user.find())
-  # for s in user.find():
-  #   output.append({'name' : s['name'], 'age' : s['age']})
-  # return jsonify({'result' : output})
+  mongo.db.rules.insert_one(akashic_rule)
+
+  return akashic_rule
 
 
 app.run(debug=True)
